bbf59ab30482b8daed1c394b27801a1c0dbaed5a
cj <cj@ss.com>
2020-05-04 22:48:43 +0800

    1.0.0

---
+++ cj.netos.ec.wallet.program/src/cj/netos/ec/wallet/cmd/QueryAmountCommand.java
@@ -0,0 +1,16 @@
+package cj.netos.ec.wallet.cmd;
+
+import cj.netos.rabbitmq.RabbitMQException;
+import cj.netos.rabbitmq.RetryCommandException;
+import cj.netos.rabbitmq.consumer.IConsumerCommand;
+import cj.studio.ecm.annotation.CjService;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Envelope;
+
+@CjService(name = "/wallet.ports#queryAmount")
+public class QueryAmountCommand implements IConsumerCommand {
+    @Override
+    public void command(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws RabbitMQException, RetryCommandException {
+        System.out.println("--------" + new String(body));
+    }
+}
