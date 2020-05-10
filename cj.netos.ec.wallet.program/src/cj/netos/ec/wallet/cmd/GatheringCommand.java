5c5cb4daece4c4eea4ca322999442d4d9f7197f2
cj <cj@ss.com>
2020-05-10 21:51:19 +0800

    1.0.0

--- cj.netos.ec.wallet.program/src/cj/netos/ec/wallet/cmd/GatheringCommand.java
+++ cj.netos.ec.wallet.program/src/cj/netos/ec/wallet/cmd/GatheringCommand.java
@@ -4,14 +4,20 @@ import cj.netos.rabbitmq.RabbitMQException;
 import cj.netos.rabbitmq.RetryCommandException;
 import cj.netos.rabbitmq.consumer.IConsumerCommand;
 import cj.studio.ecm.annotation.CjService;
+import cj.studio.ecm.annotation.CjServiceRef;
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Envelope;
+import org.apache.curator.framework.CuratorFramework;
 
 @CjService(name = "/wallet.ports#gathering")
 public class GatheringCommand implements IConsumerCommand {
+    @CjServiceRef(refByName = "curator.framework")
+    CuratorFramework framework;
+
     @Override
     public void command(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws RabbitMQException, RetryCommandException {
+//        InterProcessReadWriteLock lock = new InterProcessReadWriteLock(framework, "/test");
+//        lock.readLock().acquire();
         System.out.println("--------" + new String(body));
     }
 }
