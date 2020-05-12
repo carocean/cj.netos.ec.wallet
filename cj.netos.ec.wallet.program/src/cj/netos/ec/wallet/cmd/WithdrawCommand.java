b142e41a05bc7d4e72a9897609d6dbf61b64dd66
cj <cj@ss.com>
2020-05-12 23:03:23 +0800

    1.0.0

--- cj.netos.ec.wallet.program/src/cj/netos/ec/wallet/cmd/WithdrawCommand.java
+++ cj.netos.ec.wallet.program/src/cj/netos/ec/wallet/cmd/WithdrawCommand.java
@@ -1,16 +1,113 @@
 package cj.netos.ec.wallet.cmd;
 
+import cj.netos.ec.wallet.ICuratorPathChecker;
 import cj.netos.rabbitmq.RabbitMQException;
 import cj.netos.rabbitmq.RetryCommandException;
 import cj.netos.rabbitmq.consumer.IConsumerCommand;
+import cj.studio.ecm.IServiceSite;
 import cj.studio.ecm.annotation.CjService;
+import cj.studio.ecm.annotation.CjServiceRef;
+import cj.studio.ecm.annotation.CjServiceSite;
+import cj.studio.openport.util.Encript;
+import cj.ultimate.gson2.com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.LongString;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessReadWriteLock;
 
-@CjService(name = "/wallet.ports#withdraw")
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@CjService(name = "/wallet.ports#withdrawDone")
 public class WithdrawCommand implements IConsumerCommand {
+    @CjServiceSite
+    IServiceSite site;
+
+    @CjServiceRef(refByName = "curator.framework")
+    CuratorFramework framework;
+
+    @CjServiceRef
+    ICuratorPathChecker curatorPathChecker;
     @Override
     public void command(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws RabbitMQException, RetryCommandException {
-        System.out.println("--------" + new String(body));
+        LongString personLS = (LongString) properties.getHeaders().get("person");
+        String path = String.format("/wallet/%s/locks", personLS.toString());
+        try {
+            curatorPathChecker.check(framework, path);
+        } catch (Exception e) {
+            throw new RabbitMQException("500", e);
+        }
+        InterProcessReadWriteLock lock = new InterProcessReadWriteLock(framework, path);
+        InterProcessMutex mutex = lock.writeLock();
+        try {
+            mutex.acquire();
+            _doCmd(properties, body);
+        } catch (RabbitMQException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RabbitMQException("500", e);
+        } finally {
+            try {
+                mutex.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void _doCmd(AMQP.BasicProperties properties, byte[] body) throws RabbitMQException {
+        OkHttpClient client = (OkHttpClient) site.getService("@.http");
+
+        String appid = site.getProperty("appid");
+        String appKey = site.getProperty("appKey");
+        String appSecret = site.getProperty("appSecret");
+        String portsurl = site.getProperty("ports.oc.wallet");
+        LongString snLS = (LongString) properties.getHeaders().get("sn");
+        LongString codeLS = (LongString) properties.getHeaders().get("code");
+        LongString msgLS = (LongString) properties.getHeaders().get("message");
+        long amountLS = (long) properties.getHeaders().get("amount");
+
+        String nonce = Encript.md5(String.format("%s%s", UUID.randomUUID().toString(), System.currentTimeMillis()));
+        String sign = Encript.md5(String.format("%s%s%s", appKey, nonce, appSecret));
+
+        final Request request = new Request.Builder()
+                .url(String.format("%s?sn=%s&amount=%s&code=%s&message=%s", portsurl, snLS, amountLS,codeLS, msgLS))
+                .addHeader("Rest-Command", "withdrawDone")
+                .addHeader("app-id", appid)
+                .addHeader("app-key", appKey)
+                .addHeader("app-nonce", nonce)
+                .addHeader("app-sign", sign)
+                .get()
+                .build();
+        final Call call = client.newCall(request);
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            throw new RabbitMQException("1002", e);
+        }
+        if (response.code() >= 400) {
+            throw new RabbitMQException("1002", String.format("远程访问失败:%s", response.message()));
+        }
+        String json = null;
+        try {
+            json = response.body().string();
+        } catch (IOException e) {
+            throw new RabbitMQException("1002", e);
+        }
+        Map<String, Object> map = new Gson().fromJson(json, HashMap.class);
+        if (Double.parseDouble(map.get("status") + "") >= 400) {
+            throw new RabbitMQException(map.get("status") + "", map.get("message") + "");
+        }
+        json = (String) map.get("dataText");
+        map = new Gson().fromJson(json, HashMap.class);
     }
 }
